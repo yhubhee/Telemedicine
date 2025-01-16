c40d41c21ceb67678fc03232ff6ee3a1b5e993f4
yhubhee <142858806+yhubhee@users.noreply.github.com>
2025-01-16 04:40:50 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,52 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-const {Pool} = require('pg');
 
 
-// const dbconfig = databaseUrl.parse(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE,
+    port: process.env.Port,    
+})
 
-// Check if the URL is valid
-// Use the parsed values for a MySQL connection
-const pool = new Pool({
-  host: config.DATABASE_HOST,
-  user: config.DATABASE_USER,
-  password: config.DATABASE_PASS,
-  database: config.DATABASE,
-  port: config.Port,
-  max: 10, // Maximum number of connections in the pool
-  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-  connectionTimeoutMillis: 2000,
-});
-
-pool.connect((err, client, release)=>{
+db.connect((err)=>{
     if (err){
-        console.error("Not working")
+        console.log(err)
     }else{
-        console.log("working");
-        release(); 
+        console.log("working")
     }
 })
 
-module.exports = pool
 
-
-// const db = mysql.createConnection({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASS,
-//     database: process.env.DATABASE,
-//     port: process.env.Port,    
-// })
-
-// db.connect((err)=>{
-//     if (err){
-//         console.log(err)
-//     }else{
-//         console.log("working")
-//     }
-// })
-
-
-// module.exports = db
+module.exports = db
 
 
