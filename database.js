28412fd386db02b5ebd867e08df650a4c255eebf
yhubhee <142858806+yhubhee@users.noreply.github.com>
2025-01-16 02:32:54 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,55 +1,54 @@
 const mysql = require('mysql2');
-const databaseUrl = require('database-url');
+// const databaseUrl = require('database-url');
 require('dotenv').config();
 
 
-const config = databaseUrl.parse(process.env.DATABASE_URL);
+// const config = databaseUrl.parse(process.env.DATABASE_URL);
 
-console.log(config);
+// console.log(config);
 
-// Use the parsed values for a MySQL connection
-const db = mysql.createPool({
-  host: config.host,
-  user: config.username,
-  password: config.password,
-  database: config.database,
-  port: config.port,
-  waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    connectTimeout: 90000 
-});
+// // Use the parsed values for a MySQL connection
+// const db = mysql.createPool({
+//   host: config.host,
+//   user: config.username,
+//   password: config.password,
+//   database: config.database,
+//   port: config.port,
+//   waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     connectTimeout: 90000 
+// });
 
-db.getConnection((err, connection)=>{
-    if (err){
-        console.log(err)
-    }else{
-        console.log("working");
-        connection.release(); 
-    }
-})
-
-module.exports = db.promise()
-
-
-
-// const db = mysql.createConnection({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASS,
-//     database: process.env.DATABASE,
-//     port: process.env.PORT,    
-// })
-
-// db.connect((err)=>{
+// db.getConnection((err, connection)=>{
 //     if (err){
 //         console.log(err)
 //     }else{
-//         console.log("working")
+//         console.log("working");
+//         connection.release(); 
 //     }
 // })
 
+// module.exports = db.promise()
 
-// module.exports = db
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE,
+    port: process.env.Port,    
+})
+
+db.connect((err)=>{
+    if (err){
+        console.log(err)
+    }else{
+        console.log("working")
+    }
+})
+
+
+module.exports = db
 
 
