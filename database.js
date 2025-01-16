b1bfe54d122bac7937711c93aae5e8bd14aef469
yhubhee <142858806+yhubhee@users.noreply.github.com>
2025-01-16 02:28:24 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,33 +1,55 @@
 const mysql = require('mysql2');
-// const fs = require('fs');
+const databaseUrl = require('database-url');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE,
-    port: process.env.PORT 
-//     connectionLimit: 10,
-//     connectTimeout: 20000,
-//     multipleStatements: true,
-//     ssl: {
-//         ca: fs.readFileSync('./ca.pem')
-//     },
-// enableKeepAlive: true,
-// keepAliveInitialDelay: 10000
-    
-})
 
-db.connect((err)=>{
+const config = databaseUrl.parse(process.env.DATABASE_URL);
+
+console.log(config);
+
+// Use the parsed values for a MySQL connection
+const db = mysql.createPool({
+  host: config.host,
+  user: config.username,
+  password: config.password,
+  database: config.database,
+  port: config.port,
+  waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 90000 
+});
+
+db.getConnection((err, connection)=>{
     if (err){
         console.log(err)
     }else{
-        console.log("working")
+        console.log("working");
+        connection.release(); 
     }
 })
 
+module.exports = db.promise()
 
-module.exports = db
+
+
+// const db = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASS,
+//     database: process.env.DATABASE,
+//     port: process.env.PORT,    
+// })
+
+// db.connect((err)=>{
+//     if (err){
+//         console.log(err)
+//     }else{
+//         console.log("working")
+//     }
+// })
+
+
+// module.exports = db
 
 
